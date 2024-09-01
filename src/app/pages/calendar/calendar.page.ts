2b56f269842e2f06d21d087ee490adced5e3bf1a
ke.sotof <ke.sotof@duocuc.cl>
2024-08-31 20:44:42 -0400

    Calendario agregado de toast

--- src/app/pages/calendar/calendar.page.ts
+++ src/app/pages/calendar/calendar.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-calendar',
@@ -8,9 +9,19 @@ import { Router } from '@angular/router';
 })
 export class CalendarPage implements OnInit {
 
-  constructor( private router:Router) {}
+  constructor( private router:Router, private toastController: ToastController) {}
   navigate(){
   this.router.navigate(['/calendario'])
+  }
+
+  async presentToast(position:'middle') {
+    const toast = await this.toastController.create({
+      message: 'Esta funcion estara disponible en futuras actualizaciones!',
+      duration: 1700,
+      position: position,
+    });
+
+    await toast.present();
   }
 
   ngOnInit() {
