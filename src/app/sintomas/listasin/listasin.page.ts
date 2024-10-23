ee15f18baa713bd8679f1e888201acf80e7b3155
kesotof <ke.sotof@duocuc.cl>
2024-10-23 15:32:48 -0300

    Agregado cloud sintomas

--- src/app/sintomas/listasin/listasin.page.ts
+++ src/app/sintomas/listasin/listasin.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SintomasService } from 'src/service/sintomas.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listasin',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listasin.page.scss'],
 })
 export class ListasinPage implements OnInit {
+  sintomas: any[] = [];
 
-  constructor() { }
+  constructor( private sintomasService : SintomasService, private router : Router ) { }
 
   ngOnInit() {
+    this.sintomasService.getSintomas().subscribe(data => {
+      this.sintomas = data;
+    });
   }
-
+  
+  verDetalle(id: string) {
+    if (id) {
+      this.router.navigate(['/sintoma-detalle', id]);
+    } else {
+      console.error('ID de sintoma no definido');
+    }
+  }
 }
