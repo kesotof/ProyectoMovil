fbbe6268397d59522df0cc26623c2559497bd804
Eduardo Kusar <21264573+EduardoKQ@users.noreply.github.com>
2024-11-05 20:57:26 -0300

    another one

--- src/interfaces/Pastillero.ts
+++ src/interfaces/Pastillero.ts
@@ -7,7 +7,11 @@ export interface Pastillero {
     }[];
 }
 
-
+// Create a new Pastillero object
+// let myPastillero: Pastillero = {
+//     pastilleroId: '123',
+//     pastillas: []
+// };
 
 // Create a new pastilla
 // let newPastilla = {
