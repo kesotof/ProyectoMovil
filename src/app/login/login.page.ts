0feba1eb77d3b323f15ea2ef993ec902d65785c2
Eduardo Kusar <21264573+EduardoKQ@users.noreply.github.com>
2024-11-05 11:25:24 -0300

    reestructura cositas

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { SessionManager } from '../../managers/SessionManager';
 import { AlertManager } from '../../managers/AlertManager';
-import { StorageProvider } from 'src/managers/StorageProvider';
-import { getDatabase, ref, get } from 'firebase/database';
+import { LoginUserUseCase } from 'src/use-cases/login-user.usecase';
 
 @Component({
   selector: 'app-login',
@@ -16,9 +14,8 @@ export class LoginPage implements OnInit {
 
   constructor(
     private router: Router,
-    private sessionManager: SessionManager,
     private alertManager: AlertManager,
-    private storageProvider: StorageProvider
+    private loginUserUseCase: LoginUserUseCase
   ) { }
 
   ngOnInit() {
@@ -31,27 +28,39 @@ export class LoginPage implements OnInit {
   }
 
   async onLoginButtonClick() {
+    // try {
+    //   const userCredential = await this.sessionManager.loginWithEmail(this.email, this.password);
+    //   const user = userCredential.user;
+    //   if (user) {
+    //     const db = getDatabase();
+    //     const userRef = ref(db, `users/${user.uid}`);
+    //     const snapshot = await get(userRef);
+    //     if (snapshot.exists()) {
+    //       const userData = snapshot.val();
+    //       const username = userData.username;
+    //       await this.storageProvider.set('currentUsername', username);
+    //       await this.storageProvider.set('session', true);
+    //       this.router.navigate(['/splash']);
+    //     } else {
+    //       this.alertManager.showAlert('Error', 'No se encontró información del usuario');
+    //     }
+    //   }
+    // } catch (error) {
+    //   this.alertManager.showAlert('Error', 'No se pudo iniciar sesión, intenta de nuevo.');
+    // }
+
+    // login using firebase auth service
     try {
-      const userCredential = await this.sessionManager.loginWithEmail(this.email, this.password);
-      const user = userCredential.user;
-      if (user) {
-        const db = getDatabase();
-        const userRef = ref(db, `users/${user.uid}`);
-        const snapshot = await get(userRef);
-        if (snapshot.exists()) {
-          const userData = snapshot.val();
-          const username = userData.username;
-          await this.storageProvider.set('currentUsername', username);
-          await this.storageProvider.set('session', true);
-          this.router.navigate(['/splash']);
-        } else {
-          this.alertManager.showAlert('Error', 'No se encontró información del usuario');
-        }
+      let result = await this.loginUserUseCase.login(this.email, this.password);
+      if(result){
+        console.log('Login successful');
       }
-    } catch (error) {
-      this.alertManager.showAlert('Error', 'No se pudo iniciar sesión, intenta de nuevo.');
-    }
-  }
+      else{
+        console.log('Login failed');
+      }
+  } catch (error) {
+      console.log(error);
+  }}
 
   onRegisterButtonClick() {
     this.router.navigate(['/registro']);
