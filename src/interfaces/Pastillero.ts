28df181720a96845e713ca9f5df5077269969f2e
Eduardo Kusar <21264573+EduardoKQ@users.noreply.github.com>
2024-11-05 20:56:17 -0300

    interfaz pastillas

--- src/interfaces/Pastillero.ts
+++ src/interfaces/Pastillero.ts
@@ -1,3 +1,19 @@
 export interface Pastillero {
-    pastilleroId: string|null;
+    pastilleroId: string | null;
+    pastillas: {
+        id_pastilla: string;
+        nombre: string;
+        hora: string;
+    }[];
 }
+
+
+
+// Create a new pastilla
+// let newPastilla = {
+//     id_pastilla: '001',
+//     nombre: 'Aspirin',
+//     hora: '08:00'
+// };
+
+// myPastillero.pastillas.push(newPastilla);
