8a2f3c1bb049a87737ff1806a9a3446e96c4f57b
ke.sotof <ke.sotof@duocuc.cl>
2024-11-06 13:11:01 -0300

    Eliminado del pastillero del localStorage

--- src/use-cases/logout-user.usecase.ts
+++ src/use-cases/logout-user.usecase.ts
@@ -1,13 +1,15 @@
 import { Injectable } from "@angular/core";
 import { SessionManager } from "src/service/session.controller";
-
+import { LocalStorage } from "src/service/localstorage.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class LogoutUserUseCase {
     constructor(
-        private sessionManager: SessionManager) { }
+        private sessionManager: SessionManager,
+        private localStorage: LocalStorage
+    ) { }
 
     async logout(): Promise<boolean> {
         try {
@@ -16,13 +18,12 @@ export class LogoutUserUseCase {
             if (!logoutResult) {
                 throw Error("Fallo en el logout en sessionManager");
             }
+            // Elimina el pastillero del local storage
+            await this.localStorage.remove('pastillero');
             return true;
-
-        }
-        catch (error) {
+        } catch (error) {
             console.error('Logout UC error:', error);
             return false;
         }
-
     }
 }
