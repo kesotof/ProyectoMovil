ed9769cfbbf33a21e395c056dd1787126e7e3b55
Eduardo Kusar <21264573+EduardoKQ@users.noreply.github.com>
2024-11-06 13:32:48 -0300

    .

---
+++ src/managers/SessionManager.ts
@@ -0,0 +1,77 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SessionManager {
+  private userCredentials = [
+    {
+        username: "user",
+        password: "pass",
+        email: "user@test.com"
+    },
+];
+
+private activeUser: { username: string, password: string, email: string } | null;
+private isUserActive: boolean;
+
+constructor() {
+    this.isUserActive = false;
+    this.activeUser = null;
+}
+
+public login(username: string, password: string): boolean {
+    // check if username and password are correct
+    for (const user of this.userCredentials) {
+        if (user.username === username && user.password === password) {
+            this.activeUser = user;
+            return true;
+        }
+    }
+    return false;
+}
+
+public logout(): boolean {
+    this.activeUser = null;
+    return true;
+}
+
+public register(username: string, password: string, email: string): boolean {
+    console.log("username: "+username);
+    console.log("email: "+email);
+    console.log("passw: "+password);
+    
+    // check if username is already taken
+    for (const user of this.userCredentials) {
+        if (user.username === username) {
+            return false;
+        }
+    }
+
+    // add new user
+    let newUser = {
+        username: username,
+        password: password,
+        email: email
+    }
+    this.userCredentials.push(newUser);
+    return true;
+}
+
+getUserData(){
+    if (!this.activeUser) {
+        return null;
+    }
+
+    return {
+        email: this.activeUser.email,
+        username: this.activeUser.username
+    }
+}
+
+isUserLoggedIn(): boolean {
+    console.log("isUserActive: "+this.isUserActive);
+    return this.isUserActive;
+}
+
+}
