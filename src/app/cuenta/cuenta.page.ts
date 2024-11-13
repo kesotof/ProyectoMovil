900282fe4e12d9d046c0ec29b07d444c3134d1ae
ke.sotof <ke.sotof@duocuc.cl>
2024-11-13 03:04:00 -0300

    Caso de uso de cambiar nombre del usuario y implementacion de supabase (Funcional)

--- src/app/cuenta/cuenta.page.ts
+++ src/app/cuenta/cuenta.page.ts
@@ -1,23 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LogoutUserUseCase } from 'src/use-cases/logout-user.usecase';
+import { UpdateUserNameUseCase } from 'src/use-cases/updateName-user.usecase';
 
 @Component({
   selector: 'app-cuenta',
   templateUrl: './cuenta.page.html',
   styleUrls: ['./cuenta.page.scss'],
 })
-export class CuentaPage {
-  constructor(private router: Router, private logoutUserUC: LogoutUserUseCase) { }
+export class CuentaPage implements OnInit {
+  newUserName: string = '';
+  currentUserName: string | null = '';
+
+  constructor(
+    private router: Router,
+    private logoutUserUC: LogoutUserUseCase,
+    private updateUserNameUC: UpdateUserNameUseCase
+  ) {}
+
+  ngOnInit() {
+    this.loadUserName();
+  }
+
+  async loadUserName() {
+    this.currentUserName = await this.updateUserNameUC.getUserName();
+    this.newUserName = this.currentUserName || '';
+  }
+
+  async updateUserName() {
+    try {
+      const result = await this.updateUserNameUC.updateUserName(this.newUserName);
+      if (result) {
+        console.log('Nombre de usuario actualizado correctamente');
+      } else {
+        console.error('Error al actualizar el nombre de usuario');
+      }
+    } catch (error) {
+      console.error('Error al actualizar el nombre de usuario:', error);
+    }
+  }
 
   async onLogout() {
-    // remove the user from the session
     try {
       let logoutResult = await this.logoutUserUC.logout();
       if (!logoutResult) {
         throw Error("Algo fallo!");
-      }
-      else {
+      } else {
         this.router.navigate(['/splash']);
       }
     } catch (error) {
